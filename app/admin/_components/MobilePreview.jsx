ff6b56f52dd395d3982305f0b99f19f77ba65f93
Varad Manegopale <127626309+httpsvarad@users.noreply.github.com>
2024-09-13 12:37:04 +0530

    Update MobilePreview.jsx

--- app/admin/_components/MobilePreview.jsx
+++ app/admin/_components/MobilePreview.jsx
@@ -7,7 +7,7 @@ const MobilePreview = () => {
     <div>
         <div className='border-[13px] border-[#313131] rounded-[30px] h-[550px] md:h-screen m-3 shadow-md shadow-yellow-200 '>
 
-        <iframe className='h-full w-full rounded-[20px]'  src={process.env.NEXT_PUBLIC_BASE_URL+"httpsvarad"}></iframe>
+        <iframe className='h-full w-full rounded-[20px]'  src="https://ninjaxportfolio.vercel.app/httpsvarad"></iframe>
 
         </div>
     </div>
